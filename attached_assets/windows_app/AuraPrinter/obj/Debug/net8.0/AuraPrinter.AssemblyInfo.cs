ea2b5f6e86bca69370a961abc0ed136d1e9ca35a
Your Name <you@example.com>
2026-09-09 23:19:53 -0300

    chore: remove Replit configs, clean old launchers, keep local Electron setup

--- attached_assets/windows_app/AuraPrinter/obj/Debug/net8.0/AuraPrinter.AssemblyInfo.cs
+++ attached_assets/windows_app/AuraPrinter/obj/Debug/net8.0/AuraPrinter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuraPrinter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b496dc3e96fa6968147c385b72085a33b7f50a0d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b087be24817e5de59ea065cbe1672ea64fd0dade")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuraPrinter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuraPrinter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
