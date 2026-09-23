3269703205b1942df71e2bc3270d64ffbcaa774b
SkelleTu <166669258+SkelleTu@users.noreply.github.com>
2026-09-22 21:01:45 -0300

    Proteger React com Error Boundary monitorado

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,7 +2,12 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { installAuraRuntimeMonitor } from "./lib/runtimeMonitor";
+import { AuraRuntimeErrorBoundary } from "./lib/AuraRuntimeErrorBoundary";
 
 installAuraRuntimeMonitor();
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <AuraRuntimeErrorBoundary>
+    <App />
+  </AuraRuntimeErrorBoundary>,
+);
