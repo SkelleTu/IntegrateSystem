86a008700353838db0aefff9bda46bce4fc93287
SkelleTu <166669258+SkelleTu@users.noreply.github.com>
2026-09-22 21:01:13 -0300

    Ativar monitor global antes da interface React

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,8 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { installAuraRuntimeMonitor } from "./lib/runtimeMonitor";
+
+installAuraRuntimeMonitor();
 
 createRoot(document.getElementById("root")!).render(<App />);
